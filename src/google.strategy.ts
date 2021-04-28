90a10951264a892fd2d02e7f7f31c3be8a89fe8a
pablo <perez51160900@hotmail.com>
2021-04-27 22:36:41 -0400

    initial comit

---
+++ src/google.strategy.ts
@@ -0,0 +1,15 @@
+import {PassportStrategy} from "@nestjs/passport";  
+import {Strategy, VerifyCallback} from 'passport-google-oauth20';
+import {Injectable} from "@nestjs/common"; 
+
+@Injectable()
+
+export class GoogleStrategy extends PassportStrategy (Strategy, 'google'){
+    constructor(){
+        super({
+            clientID:'', 
+            clientSecret:'', 
+            callbackUrl:""
+        })
+    }
+}
